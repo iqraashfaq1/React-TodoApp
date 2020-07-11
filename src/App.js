a2b22c3bf58353d3d37b9743ebdb59a353fe6323
Iqra Ashfaq <iqra.ashfaq02@gmail.com>
2020-07-12 02:26:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+        <h1>Changes locally can check its push or not</h1>
       </header>
     </div>
   );
